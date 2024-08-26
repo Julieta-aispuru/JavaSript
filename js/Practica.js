3864496e350c744c512a0eb6f92a5a8b5f756e27
Julieta Aispuru <ju_aispuru@hotmail.com>
2024-08-26 20:30:55 -0300

    trabajo practico 2

--- js/Practica.js
+++ js/Practica.js
@@ -1,30 +1,22 @@
 
+const botonesAgregar = document.querySelectorAll('.agregar-carrito');
+const listaCarrito = document.getElementById('lista-carrito');
+const totalElemento = document.getElementById('total');
+let total = 0;
 
-function getPriceAndDiscount() {
-    const precioOriginal = parseFloat(prompt("Introduce el precio original del producto:"));
-    const porcentajeDescuento = parseFloat(prompt("Introduce el porcentaje de descuento:"));
-    return { precioOriginal, porcentajeDescuento };
-}
+botonesAgregar.forEach(boton => {
+    boton.addEventListener('click', () => {
+  
+        const producto = boton.closest('.producto');
+        const nombre = producto.getAttribute('data-nombre');
+        const precio = parseFloat(producto.getAttribute('data-precio'));
 
-function calculateDiscountedPrice(precioOriginal, porcentajeDescuento) {
-    const descuento = (precioOriginal * porcentajeDescuento) / 100;
-    const precioFinal = precioOriginal - descuento;
-    return precioFinal;
-}
 
-function showFinalPrice(precioFinal) {
-    alert(`Precio final con descuento es: ${precioFinal}`);
-}
+        const itemCarrito = document.createElement('li');
+        itemCarrito.textContent = `${nombre} - $${precio}`;
+        listaCarrito.appendChild(itemCarrito);
 
-function calculofinal() {
-    const { precioOriginal, porcentajeDescuento } = getPriceAndDiscount();
-
-    if ((precioOriginal) && (porcentajeDescuento)) {
-        const precioFinal = calculateDiscountedPrice(precioOriginal, porcentajeDescuento);
-        showFinalPrice(precioFinal);
-    } else {
-        alert("Introduzca valores numéricos válidos.");
-    }
-}
-
-calculofinal();
+        total += precio;
+        totalElemento.textContent = `Total: $${total}`;
+    });
+});
